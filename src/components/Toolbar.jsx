d8549f578ad65affcae85088515dedb655fcd6d8
nacukisubaru <alexitsheff@gmail.com>
2022-05-22 19:29:57 +0300

    ластик

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -5,6 +5,7 @@ import toolState from "../store/toolState";
 import canvasState from "../store/canvasState";
 import Brush from "../tools/Brush";
 import Rect from "../tools/Rect";
+import Eraser from "../tools/Eraser";
 
 const changeColor = e => {
     toolState.setStrokeColor(e.target.value);
@@ -17,7 +18,7 @@ const ToolBar = observer(()=> {
             <button className="toolbar-btn brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
             <button className="toolbar-btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></button>
             <button className="toolbar-btn circle"></button>
-            <button className="toolbar-btn eraser"></button>
+            <button className="toolbar-btn eraser" onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}></button>
             <button className="toolbar-btn line"></button>
             <input onChange={e => changeColor(e)} style={{marginLeft:'5px'}} type="color"></input>
             <button className="toolbar-btn undo"></button>
