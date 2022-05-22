4f3823499321260edcf62150f6b3a23cf8df6d97
nacukisubaru <alexitsheff@gmail.com>
2022-05-22 17:03:36 +0300

    стили + компоненты

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/app.scss";
+import ToolBar from "./components/Toolbar";
+import SettingBar from "./components/SettingBar";
+import Canvas from "./components/Canvas";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <ToolBar></ToolBar>
+            <SettingBar></SettingBar>
+            <Canvas></Canvas>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/toolbar.scss';
+
+const ToolBar = () => {
+    return (
+        <div className="toolbar">
+            <button className="toolbar-btn brush"></button>
+            <button className="toolbar-btn rect"></button>
+            <button className="toolbar-btn circle"></button>
+            <button className="toolbar-btn eraser"></button>
+            <button className="toolbar-btn line"></button>
+            <input style={{marginLeft:'5px'}} type="color"></input>
+            <button className="toolbar-btn undo"></button>
+            <button className="toolbar-btn redo"></button>
+            <button className="toolbar-btn save"></button>
+        </div>
+    )
+}
+
+export default ToolBar;
